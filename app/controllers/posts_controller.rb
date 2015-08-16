04bca0998c94c9de96f12bbb2bac28f5ca4c0f1a
Satoshi Ebisawa <e.satoshi@gmail.com>
2015-08-16 15:39:16 +0900

    Add create form with design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_url
+    else
+      @posts = Post.all
+      render :index
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
